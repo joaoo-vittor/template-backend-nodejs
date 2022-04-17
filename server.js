50bd28291f45398fcb3803c301fdd5461666442e
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-04-17 17:08:34 -0300

    fix ports

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const port = Number(process.env.PORT) || 3001;
-const host = process.env.HOST || "localhost";
+const host = process.env.HOST || "0.0.0.0";
 
 app.listen(port, host, () => {
   console.log(`CTRL + Click em http://${host}:${port}`);
